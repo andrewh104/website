eec7e8832097fb4bebdaa435b7a8709753c41f92
andrewh104 <andrewh1048596@gmail.com>
2023-02-10 17:13:38 -0500

    Update sidebar.js

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -1,9 +1,8 @@
 import { React, useState} from 'react';
 import { Box, Button, Typography } from '@mui/material';
-import MenuIcon from '@mui/icons-material/Menu'
+import ListIcon from '@mui/icons-material/List'
 import HomeIcon from '@mui/icons-material/Home';
 import CategoryIcon from '@mui/icons-material/Category';
-
 
 
 
@@ -13,7 +12,8 @@ export const Sidebar = ({handleShift}) => {
     // make the categories slide down
     // allow for selections
 
-    const button_sidebar = (name) => {
+    const button_sidebar = (name) => 
+    {
         return <Button sx = {{ display: "block", margin: "2vh 0vw 2vh 1vw", color: "#57201B", fontSize: "1.5vh" }}>
             <Box sx={{display:"flex", justifyContent:"left", verticalAlign:"center"}}>
                 {name === 'home'
@@ -21,7 +21,7 @@ export const Sidebar = ({handleShift}) => {
                     : <CategoryIcon sx={{fontSize: "2.5vh", color:"#57201B", verticalAlign:"center"}}/>
                 }
                 {expand
-                    ? <Typography variant="h5" sx={{marginLeft:"10px"}}>{name}</Typography>
+                    ? <Typography sx={{marginLeft:"10px", fontSize: "2.5vh"}}>{name}</Typography>
                     : <></>
                 }
             </Box>
@@ -31,35 +31,40 @@ export const Sidebar = ({handleShift}) => {
 
     const [expand, setExpand] = useState(true);
     var sidebar_width = "15vw";
+    
 
-    const expandSidebar = () => {
+    const expandSidebar = () => 
+    {
         setExpand(!expand);
         if(expand){
-            sidebar_width = "15vw";
+            sidebar_width = "1vw";
             handleShift("6vw");
         }
         else{
-            sidebar_width = "4vw";
+            sidebar_width = "3vw";
             handleShift("17vw");
         }
     }
 
-    const Sidebar = () => {
+    const Sidebar = () => 
+    {
         return <Box sx = {{
                             height: "95vh", 
                             top: "5",
                             width: {sidebar_width}, 
                             display: "block", 
                             position: "fixed",
-                            borderRight: "solid 1px"
+                            borderRight: "solid 2px",
+
                             }}
                 >
             
-            <MenuIcon sx = {{margin:"8px 0 0 8px", fontSize: "4vh", color: "#57201B"}} onClick={expandSidebar}/>
+            <ListIcon sx = {{margin:"8px 0 0 8px", fontSize: "4vh", color: "#57201B"}} onClick={expandSidebar}/>
 
             <>{button_sidebar("home")}</>
             <>{button_sidebar("categories")}</>
-            <>{button_sidebar("support")}</>
+
+        
         </Box>
     }
 
